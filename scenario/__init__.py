bf5e99bdf3535323289959b3fdd1295d67d3a668
qapples <armand.scotty@yahoo.com>
2023-06-17 19:18:18 -0400

    fix runtime errors in BaseScenarioProvider

--- scenario/__init__.py
+++ scenario/__init__.py
@@ -1,14 +1,11 @@
 import abc
 import random
 
+def _get_rand_int(len):
+    return random.randint(0, len - 1)
+
 class BaseScenarioProvider:
     __metaclass__ = abc.ABCMeta
-
-    def __init__(self, scenario_list):
-        self._scenario_list = scenario_list
-
-    def _get_rand_int(len):
-        return random.randint(0, len - 1)
     
     @abc.abstractmethod
     def get_categories(self) -> tuple[str]:
@@ -39,44 +36,44 @@ class BaseScenarioProvider:
         return
     
     def get_category(self, index) -> str:
-        return self.get_categories[index]
+        return self.get_categories()[index]
     
     def get_domain(self, index) -> str:
-        return self.get_domains[index]
+        return self.get_domains()[index]
     
     def get_entropy(self, index) -> str:
-        return self.get_entropies[index]
-    
+        return self.get_entropies()[index]
+
     def get_region(self, index) -> str:
-        return self.get_regions[index]
+        return self.get_regions()[index]
     
     def get_scope(self, index) -> str:
-        return self.get_scopes[index]
+        return self.get_scopes()[index]
     
     def get_severity(self, index) -> str:
-        return self.get_severities[index]
+        return self.get_severities()[index]
     
     def get_system(self, index) -> str:
-        return self.get_systems[index]
+        return self.get_systems()[index]
     
     
     def get_random_category(self) -> str:
-        return self.get_category(self._get_rand_int(len(self.get_categories)))
+        return self.get_category(_get_rand_int(len(self.get_categories())))
     
     def get_random_domain(self) -> str:
-        return self.get_domain(self._get_rand_int(len(self.get_domains)))
+        return self.get_domain(_get_rand_int(len(self.get_domains())))
     
     def get_random_entropy(self) -> str:
-        return self.get_entropy(self._get_rand_int(len(self.get_entropies)))
+        return self.get_entropy(_get_rand_int(len(self.get_entropies())))
     
     def get_random_region(self) -> str:
-        return self.get_region(self._get_rand_int(len(self.get_regions)))
+        return self.get_region(_get_rand_int(len(self.get_regions())))
     
     def get_random_scope(self) -> str:
-        return self.get_scope(self._get_rand_int(len(self.get_scopes)))
+        return self.get_scope(_get_rand_int(len(self.get_scopes())))
     
     def get_random_severity(self) -> str:
-        return self.get_severity(self._get_rand_int(len(self.get_severities)))
+        return self.get_severity(_get_rand_int(len(self.get_severities())))
     
     def get_random_system(self) -> str:
-        return self.get_system(self._get_rand_int(len(self.get_systems)))
+        return self.get_system(_get_rand_int(len(self.get_systems())))
